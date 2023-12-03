f9c64f2a6b2ed038ec7178b3928f990134a53f7e
siddiquifaras <123031834+siddiquifaras@users.noreply.github.com>
2023-12-04 01:12:18 +0500

    Updated index.php

--- index.php
+++ index.php
@@ -4,31 +4,33 @@
     <title>Faras' Crawler</title>
 </head>
 <body>
-    <h1>Faras' Web Crawler</h1>
+    <h1>Web Crawler</h1>
     <!--takes input url from user and crawls that-->
     <form id="crawlForm" method="POST">
-        <label for="urlInput">Enter a vaid URL:</label>
+        <label for="urlInput">Enter a URL:</label>
         <input type="text" id="urlInput" name="url" required>
-        <button type="submit">Begin Crawling</button>
+        <button type="submit">Crawl</button>
     </form>
+    <div id="result"></div>
     <?php
-    //requests url's and calls the crawlPage function and places them in an array
-     if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    //requests url's and calls the crawlPage function and places
+    if ($_SERVER["REQUEST_METHOD"] === "POST") {
         $url = $_POST["url"];
         $urls = crawlPage($url);
         $response = [
             "urls" => $urls
         ];
+        
         displayResults($response["urls"]);
         exit;
     }
     //function to crawl a url and return anchor tag content
     function crawlPage($url) {
-        $html = file_get_contents($url);  // Get the HTML content of the page
-        $pattern = '/<a\s(?:[^>]?\s)?href=(["\'])(.?)\1/'; //match anchor tag and extract href
+        $html = file_get_contents($url); // Get the HTML content of the page
+        $pattern = '/<a\s(?:[^>]?\s)?href=(["\'])(.*?)\1/'; // Regular expression pattern to match URLs
         if (preg_match_all($pattern, $html, $matches)) {
             $urls = $matches[2]; // Extract the URLs from the matched patterns
-            return $urls; //will return url
+            return $urls;
         } else {
             return [];
         }
@@ -39,14 +41,12 @@
         foreach ($urls as $url) {
             echo "<a href=\"$url\">$url</a><br>\n";
         }
-        echo "</div>\n";
-        echo "<script>\n";
-        echo "document.getElementById(\"urlInput\").value = \"" . htmlspecialchars($_POST["url"]) . "\";\n";
-        echo "</script>\n";
         echo "</body>\n";
         echo "</html>";
     }
     ?>
+</body>
+</html>
 
 </body>
 </html>
