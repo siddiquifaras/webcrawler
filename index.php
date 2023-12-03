fafc091cecb57cb0b0c69f0f51c70a078ca6ac4b
siddiquifaras <123031834+siddiquifaras@users.noreply.github.com>
2023-12-03 23:47:45 +0500

    added crawlpage function which extracts anchor tags within a url

--- index.php
+++ index.php
@@ -2,12 +2,21 @@
 <html>
 <head>
     <title>Faras' Crawler</title>
-    <
 </head>
 <body>
     <h1>Web Crawler</h1>
     <?php
-    
+    //function to crawl a url and return anchor tag content
+    function crawlPage($url) {
+        $html = file_get_contents($url);  // Get the HTML content of the page
+        $pattern = '/<a\s(?:[^>]?\s)?href=(["\'])(.?)\1/'; //match anchor tag and extract href
+        if (preg_match_all($pattern, $html, $matches)) {
+            $urls = $matches[2]; // Extract the URLs from the matched patterns
+            return $urls; //will return url
+        } else {
+            return [];
+        }
+    }
     ?>
 
 </body>
