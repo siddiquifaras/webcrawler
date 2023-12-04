ae571d51b617ea20f6e49e97533a429bfc2a6099
siddiquifaras <123031834+siddiquifaras@users.noreply.github.com>
2023-12-04 23:39:43 +0500

    added error handling and fixed some errors

--- index.php
+++ index.php
@@ -5,43 +5,53 @@
 </head>
 <body>
     <h1>Web Crawler</h1>
-    <!--takes input url from user and crawls that-->
+    <!-- Takes input URL and depth limit from the user and crawls that -->
     <form id="crawlForm" method="POST">
         <label for="urlInput">Enter a URL:</label>
         <input type="text" id="urlInput" name="url" required>
+        <br>
         <label for="depthInput">Enter the Depth Limit:</label>
         <input type="number" id="depthInput" name="depth" min="1" required>
         <button type="submit">Crawl</button>
     </form>
     <div id="result"></div>
+
     <?php
-    //requests url's and calls the crawlPage function and places
+    // Requests URL and depth limit from the user and calls the crawlPage function
     if ($_SERVER["REQUEST_METHOD"] === "POST") {
         $url = $_POST["url"];
         $depth = $_POST["depth"];
         $urls = crawlPage($url, $depth);
-        $response = [
-            "urls" => $urls
-        ];
-        
-        displayResults($response["urls"]);
+
+        if ($urls === false) {
+            echo "An error occurred while crawling the page.";
+            exit;
+        }
+
+        displayResults($urls);
         exit;
     }
-    //function to crawl a url and return anchor tag content
+
+    // Function to crawl a URL and return anchor tag content up to a specified depth level
     function crawlPage($url, $depth) {
-         if ($depth <= 0) {
-            return false; //returning false if the depth limit is not a positive number
+        if ($depth <= 0) {
+            return false; // Return false if the depth limit is not a positive number
         }
-        $html = file_get_contents($url); // Get the HTML content of the page
-        $pattern = '/<a\s(?:[^>]?\s)?href=(["\'])(.*?)\1/'; // Regular expression pattern to match URLs
+
+        $html = @file_get_contents($url); // Get the HTML content of the page and suppress errors
+        if ($html === false) {
+            return false; // Return false if there was an error fetching the page
+        }
+
+        $pattern = '/<a\s+(?:[^>]*?\s+)?href=([\'"])(.*?)\1/';
         if (preg_match_all($pattern, $html, $matches)) {
             $urls = $matches[2]; // Extract the URLs from the matched patterns
             if ($depth === 1) {
-                return $urls; //return the URLs at the current depth level
+                return $urls; // Return the URLs at the current depth level
             } else {
                 $filteredUrls = [];
                 foreach ($urls as $url) {
-                    $subUrls = crawlPage($url, $depth - 1); //recursive crawl the URLs at the next depth level
+                    $subUrls = crawlPage($url, $depth - 1); // Recursively crawl the URLs at the next depth level
                     if (!empty($subUrls)) {
                         $filteredUrls = array_merge($filteredUrls, $subUrls); // Merge the sub URLs into the main URL list
                     }
@@ -52,18 +62,18 @@
             return [];
         }
     }
-    //function to display all the urls appearing on a page
+
+    // Function to display all the URLs appearing on a page
     function displayResults($urls) {
         echo "<h2>URLs found on the page:</h2>\n";
-        foreach ($urls as $url) {
-            echo "<a href=\"$url\">$url</a><br>\n";
+        if (!empty($urls)) {
+            foreach ($urls as $url) {
+                echo "<a href=\"$url\">$url</a><br>\n";
+            }
+        } else {
+            echo "No URLs found.";
         }
-        echo "</body>\n";
-        echo "</html>";
     }
     ?>
-</body>
-</html>
-
 </body>
 </html>
