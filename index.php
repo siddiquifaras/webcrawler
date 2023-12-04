85fc235bed15035fe2f19728535d26ef0e48be96
siddiquifaras <123031834+siddiquifaras@users.noreply.github.com>
2023-12-04 23:19:51 +0500

    added depth feature

--- index.php
+++ index.php
@@ -9,6 +9,8 @@
     <form id="crawlForm" method="POST">
         <label for="urlInput">Enter a URL:</label>
         <input type="text" id="urlInput" name="url" required>
+        <label for="depthInput">Enter the Depth Limit:</label>
+        <input type="number" id="depthInput" name="depth" min="1" required>
         <button type="submit">Crawl</button>
     </form>
     <div id="result"></div>
@@ -16,7 +18,8 @@
     //requests url's and calls the crawlPage function and places
     if ($_SERVER["REQUEST_METHOD"] === "POST") {
         $url = $_POST["url"];
-        $urls = crawlPage($url);
+        $depth = $_POST["depth"];
+        $urls = crawlPage($url, $depth);
         $response = [
             "urls" => $urls
         ];
@@ -25,12 +28,26 @@
         exit;
     }
     //function to crawl a url and return anchor tag content
-    function crawlPage($url) {
+    function crawlPage($url, $depth) {
+         if ($depth <= 0) {
+            return false; //returning false if the depth limit is not a positive number
+        }
         $html = file_get_contents($url); // Get the HTML content of the page
         $pattern = '/<a\s(?:[^>]?\s)?href=(["\'])(.*?)\1/'; // Regular expression pattern to match URLs
         if (preg_match_all($pattern, $html, $matches)) {
             $urls = $matches[2]; // Extract the URLs from the matched patterns
-            return $urls;
+            if ($depth === 1) {
+                return $urls; //return the URLs at the current depth level
+            } else {
+                $filteredUrls = [];
+                foreach ($urls as $url) {
+                    $subUrls = crawlPage($url, $depth - 1); //recursive crawl the URLs at the next depth level
+                    if (!empty($subUrls)) {
+                        $filteredUrls = array_merge($filteredUrls, $subUrls); // Merge the sub URLs into the main URL list
+                    }
+                }
+                return array_unique($filteredUrls); // Use array_unique to remove duplicates from the URL list
+            }
         } else {
             return [];
         }
